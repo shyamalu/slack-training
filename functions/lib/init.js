bc5ae58e3915eab2a724ce05aaa3c4d1fb37c967
Shyamal.Upadhyaya <shyamal.upadhyaya@adesa.com>
2019-06-19 11:12:25 +0530

    removed unnecessary files

--- functions/lib/init.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const admin = require('firebase-admin');
-admin.initializeApp();
-exports.db = admin.firestore();
-//# sourceMappingURL=init.js.map
